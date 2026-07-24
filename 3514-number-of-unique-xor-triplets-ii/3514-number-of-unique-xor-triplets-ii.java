3d59b0d949ed23b65197f6132cdc40db12318859
Ashu Kumari <152001225+AshuKumari21@users.noreply.github.com>
2026-07-24 22:58:30 +0530

    Time: 1327 ms (27.27%), Space: 47.2 MB (30.91%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        
+        int n = nums.length;
+        if (n == 1) return 1;
+
+        HashSet<Integer> pairs = new HashSet<>();
+        BitSet triplets = new BitSet();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                pairs.add(nums[i] ^ nums[j]);
+            }
+        }
+
+        for (int pair : pairs) {
+            for (int num : nums) {
+                triplets.set(pair ^ num);
+            }
+        }
+
+        return triplets.cardinality();
+    
+    }
+}
